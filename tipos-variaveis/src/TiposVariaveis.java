2ebb22a4771f521d0c57afae45bb0e7a33a3f0f7
fbaziotti <fernandobaziotti@gmail.com>
2024-08-20 15:32:47 -0300

    salvando aruivo TiposVariaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+        long cpf = 11122234L;
+        float PI = 3.14F;
+        final int NUMERO = 5; // final e caixa alta = valor imutável. Torna-se uma constante
+        System.out.println("NUMERO");
     }
 }
